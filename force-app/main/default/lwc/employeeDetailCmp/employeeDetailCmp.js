854f70c229afa53eaef04bdc30b7d29db940a740
nguyen xuan cuong <nguyenxuancuong@luvina.net>
2021-04-26 14:01:12 +0700

    update Detail

---
+++ force-app/main/default/lwc/employeeDetailCmp/employeeDetailCmp.js
@@ -0,0 +1,12 @@
+import { LightningElement, api, track } from 'lwc';
+
+export default class EmployeeDetailCmp extends LightningElement {
+    @api employee;
+
+    get isHasRecord() {
+        if(this.employee != undefined && this.employee.Id != undefined) {
+            return true;
+        }
+        else return false;
+    }
+}
