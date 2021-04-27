827cdc2ef9e0afa1c6cb7dc274260675393b8768
hoang <phamminhhoang@luvina.net>
2021-04-27 08:52:02 +0700

    update Edit

--- force-app/main/default/lwc/employeeEditCmp/employeeEditCmp.js
+++ force-app/main/default/lwc/employeeEditCmp/employeeEditCmp.js
@@ -1,7 +1,13 @@
-import { LightningElement, api } from 'lwc';
+import { LightningElement, api, track } from 'lwc';
 
 export default class EmployeeEditCmp extends LightningElement {
     @api employee;
+    @track validator = {};
+
+    connectedCallback() {
+        console.log('abc');
+        this.validator = this.initValidatorData();
+    }
 
     closeModalHandler() {
         this.dispatchEvent(new CustomEvent('closeeditemployee'));
@@ -14,8 +20,43 @@ export default class EmployeeEditCmp extends LightningElement {
         this.employee = empObj;
     }
 
+    initValidatorData() {
+        return {
+            name: {isError: false, errMsg: ""},
+            email: {isError: false, errMsg: ""},
+            birthday: {isError: false, errMsg: ""}
+        }
+    }
+
+    validateInput(employee) {
+        let validObject = this.initValidatorData();
+
+        let REQ_FIELD_VALID = {
+            isError: true,
+            errMsg: "This field is required"
+        }
+
+        if (employee.Name.trim() === '' || employee.Name === undefined) {
+            validObject.name = REQ_FIELD_VALID;
+        }
+
+        if (employee.Email__c.trim() === '' || employee.Email__c === undefined) {
+            validObject.email = REQ_FIELD_VALID;
+        }
+
+        if (employee.BirthDay__c === null) {
+            validObject.birthday = REQ_FIELD_VALID;
+        }
+
+        this.validator = validObject;
+
+    }
+
     saveEmployee() {
-        console.log('saved employee: ' + this.employee.Name);
+        console.log('employee: ' + this.employee.BirthDay__c);
+        let inputEmp = Object.assign({}, this.employee);
+        this.validateInput(inputEmp);
+        
         this.dispatchEvent(new CustomEvent('savedemployee', {detail: JSON.stringify(this.employee)}));
     }
 }
